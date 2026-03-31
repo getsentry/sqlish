4788d1962991c304d77b4b85c73f837f9be76f16
George Gritsouk <989898+gggritso@users.noreply.github.com>
2026-03-31 10:36:47 -0400

    Formatting

--- src/SQLishParser.ts
+++ src/SQLishParser.ts
@@ -24,7 +24,7 @@ export class SQLishParser {
 /* oxlint-disable no-control-regex */
 const CONTROL_CHARACTERS = new RegExp(
   '[\\u0000-\\u0008\\u000B\\u000C\\u000E-\\u001F\\u007F-\\u009F]',
-  'g'
+  'g',
 );
 /* oxlint-enable no-control-regex */
 
